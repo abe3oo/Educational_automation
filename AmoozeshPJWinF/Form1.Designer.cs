f0212145692a5a46c38176379988dd0ffb39b5d8
amirreza abbaszadeh <amirreza.a560@gmail.com>
2024-03-24 23:29:18 +0330

    add clock

--- AmoozeshPJWinF/Form1.Designer.cs
+++ AmoozeshPJWinF/Form1.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             button1 = new Button();
             button2 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            clocklbl = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(570, 110);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Location = new Point(651, 147);
             button1.Name = "button1";
-            button1.Size = new Size(82, 22);
+            button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
             button1.Text = "AddUser";
             button1.UseVisualStyleBackColor = true;
@@ -45,31 +47,47 @@
             // 
             // button2
             // 
-            button2.Location = new Point(570, 169);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Location = new Point(651, 225);
             button2.Name = "button2";
-            button2.Size = new Size(82, 22);
+            button2.Size = new Size(94, 29);
             button2.TabIndex = 1;
             button2.Text = "pay";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // clocklbl
+            // 
+            clocklbl.AutoSize = true;
+            clocklbl.Location = new Point(361, 9);
+            clocklbl.Name = "clocklbl";
+            clocklbl.Size = new Size(113, 20);
+            clocklbl.TabIndex = 2;
+            clocklbl.Text = "Loading Time ...";
+            clocklbl.TextAlign = ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
+            ClientSize = new Size(839, 483);
+            Controls.Add(clocklbl);
             Controls.Add(button2);
             Controls.Add(button1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private Label clocklbl;
     }
 }
--- AmoozeshPJWinF/Form1.cs
+++ AmoozeshPJWinF/Form1.cs
@@ -2,9 +2,19 @@ namespace AmoozeshPJWinF
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer t = null;
         public Form1()
         {
             InitializeComponent();
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            t = new System.Windows.Forms.Timer();
+            t.Interval = 1000;
+            t.Tick += new EventHandler(timer1_Tick);
+            t.Enabled = true;
         }
 
         private void Adduser_Click(object sender, EventArgs e)
@@ -17,6 +27,12 @@ namespace AmoozeshPJWinF
         {
             payWindow n1 = new payWindow();
             n1.ShowDialog();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string s1 = "clocklbl";
+            clocklbl.Text = DateTime.Now.ToString();
         }
     }
 }
