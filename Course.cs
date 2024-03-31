fb4d66fe037f2bba4a76a96fbe2d629a80e526d0
nazanin <nazaninroshan81@gmail.com>
2024-03-31 18:03:45 +0330

    add Course class

---
+++ Course.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+
+namespace Amozesh
+{
+    internal class Course
+    {
+        public Course()
+        {
+
+        }
+        private long CourseID;
+        private DateTime DateOfStart;
+        private long Cost;
+        private long TeacherID;
+        private string CourseName;
+
+        public long courseid { get { return CourseID; } set { CourseID = value; } }
+        public DateTime dateofstart { get { return DateOfStart; } set { DateOfStart = value; } }
+        public long cost { get { return Cost; } set { Cost = value; } }
+        public long teacherid { get { return TeacherID; } set { TeacherID = value; } }
+        public string  coursename { get { return CourseName; } set { CourseName = value; } }
+        
+
+    }
+}
