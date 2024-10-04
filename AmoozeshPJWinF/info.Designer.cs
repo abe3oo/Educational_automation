2e873a2b3d863e552bea9e207bca7a6eb89ed8ec
amirreza abbaszadeh <amirreza.a560@gmail.com>
2024-10-04 18:22:42 +0330

    fix info

--- AmoozeshPJWinF/info.Designer.cs
+++ AmoozeshPJWinF/info.Designer.cs
@@ -43,7 +43,7 @@
             label1.AutoSize = true;
             label1.BackColor = SystemColors.ControlLight;
             label1.Font = new Font("B Nazanin", 10.8F, FontStyle.Bold);
-            label1.Location = new Point(121, 177);
+            label1.Location = new Point(143, 178);
             label1.Name = "label1";
             label1.Size = new Size(129, 29);
             label1.TabIndex = 0;
@@ -54,7 +54,7 @@
             label2.AutoSize = true;
             label2.BackColor = SystemColors.ControlLight;
             label2.Font = new Font("Comic Sans MS", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(128, 223);
+            label2.Location = new Point(150, 224);
             label2.Name = "label2";
             label2.Size = new Size(124, 25);
             label2.TabIndex = 1;
@@ -65,7 +65,7 @@
             label3.AutoSize = true;
             label3.BackColor = SystemColors.ControlLight;
             label3.Font = new Font("B Nazanin", 10.8F, FontStyle.Bold);
-            label3.Location = new Point(47, 267);
+            label3.Location = new Point(69, 268);
             label3.Name = "label3";
             label3.Size = new Size(281, 29);
             label3.TabIndex = 2;
@@ -76,7 +76,7 @@
             label4.AutoSize = true;
             label4.BackColor = SystemColors.ControlLight;
             label4.Font = new Font("Comic Sans MS", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(71, 310);
+            label4.Location = new Point(93, 311);
             label4.Name = "label4";
             label4.Size = new Size(224, 25);
             label4.TabIndex = 3;
@@ -87,15 +87,16 @@
             label5.AutoSize = true;
             label5.BackColor = Color.Gainsboro;
             label5.Font = new Font("B Nazanin", 9F, FontStyle.Regular, GraphicsUnit.Point, 178);
-            label5.Location = new Point(38, 396);
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(12, 392);
             label5.Name = "label5";
-            label5.Size = new Size(281, 23);
+            label5.Size = new Size(372, 23);
             label5.TabIndex = 4;
-            label5.Text = "تمامی حقوق این اپلیکیشن برای سازندگان محفوظ میباشد";
+            label5.Text = "تمامی حقوق مادی و معنوی این اپلیکیشن برای سازندگان آن محفوظ میباشد.";
             // 
             // pictureBox1
             // 
-            pictureBox1.Location = new Point(121, 43);
+            pictureBox1.Location = new Point(143, 44);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(125, 105);
             pictureBox1.TabIndex = 5;
@@ -107,7 +108,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(359, 450);
+            ClientSize = new Size(396, 450);
             Controls.Add(pictureBox1);
             Controls.Add(label5);
             Controls.Add(label4);
@@ -115,6 +116,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
             Name = "info";
             Text = "درباره ما";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
